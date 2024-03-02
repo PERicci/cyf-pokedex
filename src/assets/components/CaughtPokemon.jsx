e04391d0c1b1900fe6d8c5e2776bc7e5ecdf2ee3
Pedro <riccip@outlook.com>
2024-03-02 14:58:46 +0000

    add pokemon button implemented

--- src/assets/components/CaughtPokemon.jsx
+++ src/assets/components/CaughtPokemon.jsx
@@ -1,24 +1,54 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 CaughtPokemon.propTypes = {
   date: PropTypes.string.isRequired,
 };
 
 export function CaughtPokemon(props) {
-  let [caught, setCaught] = useState(0);
+  const [caught, setCaught] = useState([]);
+
+  useEffect(() => {}, [caught]);
+
+  function chooseRandomPokemon() {
+    const pokemon = [
+      `Mewtwo`,
+      `Mew`,
+      `Pikachu`,
+      `Eevee`,
+      `Charizard`,
+      `Blastoise`,
+      `Alakazam`,
+      `Gengar`,
+      `Dragonair`,
+      `Dragonite`,
+      `Venusaur`,
+      `Squirtle`,
+      `Charmander`,
+      `Bulbasaur`,
+      `Pidgeotto`,
+      `Rattata`,
+    ];
+    const randomIndex = Math.floor(Math.random() * pokemon.length);
+    return pokemon[randomIndex];
+  }
 
   function catchPokemon() {
-    setCaught((prev) => prev + 1);
+    setCaught((prev) => [...prev, chooseRandomPokemon()]);
   }
 
   const { date } = props;
   return (
     <>
       <p>
-        Caught {caught} Pokemon on {date}
+        Caught {caught.length} Pokemon on {date}
       </p>
       <button onClick={catchPokemon}>Catch more one!</button>
+      <ul id="pokemon-list">
+        {caught.map((pokemon, index) => (
+          <li key={pokemon + index}>{pokemon}</li>
+        ))}
+      </ul>
     </>
   );
 }
